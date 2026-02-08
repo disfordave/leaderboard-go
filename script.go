88baabc01d464130d517cfbb9f0cf38f8688f8f4
Dave <118544840+disfordave@users.noreply.github.com>
2026-02-08 19:14:59 +0900

    implement basic HTTP server with a greeting endpoint

--- script.go
+++ script.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, Go!")
+	})
+
+	fmt.Println("Server is starting on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
